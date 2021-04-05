5846fca4c92c83959898e5bd49ffa37f90befe7b
minchankim <doridorism@naver.com>
2021-04-05 17:51:52 +0900

    asdadad

--- testblescan.py
+++ testblescan.py
@@ -6,6 +6,7 @@ import sys
 
 import bluetooth._bluetooth as bluez
 
+import time
 dev_id = 0
 try:
 	sock = bluez.hci_open_dev(dev_id)
@@ -20,7 +21,16 @@ blescan.hci_enable_le_scan(sock)
 
 while True:
 	returnedList = blescan.parse_events(sock, 10)
-	print "----------"
+	#print "----------"
 	for beacon in returnedList:
-		print beacon
-
+            mac_addr = beacon.split(',')
+            if "00:c0:b1:c0:2a:d5"  in  mac_addr[0] :
+                    print(beacon)
+					#print((float(-58-int(mac_addr[5]))/20))
+					print(round(10**(float(-69-int(mac_addr[5]))/20),2))
+                    #print(mac_addr[0])
+                    #print('finds')
+			if "00:c0:b1:c0:2a:d5"  in  mac_addr[0] or "00:c0:b1:c0:2a:d8"  in  mac_addr[0]:
+				    #print(beacon)
+					#print(mac_addr[5])
+					#print('finds')
