164b744f4210eb2448e5c8369fbadd90137af8ed
minchankim <doridorism@naver.com>
2021-04-06 17:22:32 +0900

    sfsfsdf

--- testblescan.py
+++ testblescan.py
@@ -20,17 +20,26 @@ blescan.hci_le_set_scan_parameters(sock)
 blescan.hci_enable_le_scan(sock)
 
 while True:
-	returnedList = blescan.parse_events(sock, 10)
+	returnedList = blescan.parse_events(sock, 5)
 	#print "----------"
 	for beacon in returnedList:
             mac_addr = beacon.split(',')
-            if "00:c0:b1:c0:2a:d5"  in  mac_addr[0] :
-                    print(beacon)
-					#print((float(-58-int(mac_addr[5]))/20))
-					print(round(10**(float(-69-int(mac_addr[5]))/20),2))
-                    #print(mac_addr[0])
+            """if "00:c0:b1:c0:2a:d5" in mac_addr[0]  :
+                    #print(beacon)
+                    #print((float(-58-int(mac_addr[5]))/20))
+                    a ="m"
+                    b ="(kmc-desk)"
+                    print round(10**(float(-65-int(mac_addr[5]))/20),2),a,b"""
+            if "00:c0:b1:c0:2a:d8" in mac_addr[0]  :
+                    a ="m"
+                    b ="(nyk-desk)"
+                    print round(10**(float(-65-int(mac_addr[5]))/20),2),a,b
+                    #print(mac_addr[5])
                     #print('finds')
-			if "00:c0:b1:c0:2a:d5"  in  mac_addr[0] or "00:c0:b1:c0:2a:d8"  in  mac_addr[0]:
-				    #print(beacon)
-					#print(mac_addr[5])
-					#print('finds')
+            #if "00:c0:b1:c0:2a:d5" in  mac_addr[0] or "00:c0:b1:c0:2a:d8" in mac_addr[0] :
+             #       print(beacon)
+              #      print(10^(-58-int(mac_addr[5]))/20)
+                    #print(mac_addr[5])
+                    #print('finds')
+
+
